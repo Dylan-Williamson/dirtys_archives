9773c08babb66684b1953163002148405f311cb5
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-11 09:13:24 -0400

    configures beats table

--- db/migrate/20200611130331_create_users_table.rb
+++ db/migrate/20200611130331_create_users_table.rb
@@ -3,5 +3,6 @@ class CreateUsersTable < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+    end
   end
 end
--- db/migrate/20200611130351_create_beats_table.rb
+++ db/migrate/20200611130351_create_beats_table.rb
@@ -1,4 +1,9 @@
 class CreateBeatsTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :beats do |t|
+      t.string :title
+      t.integer :user_id
+      t.integer :genre_id
+    end
   end
 end
