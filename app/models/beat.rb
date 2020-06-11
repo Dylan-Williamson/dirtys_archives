d6874ecc6034dbb968eebd9e6b474b09e9687fb5
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-11 14:06:37 -0400

    overrides .to_s for Beat class

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -3,5 +3,8 @@ class Beat < ActiveRecord::Base
     belongs_to :user
     has_many :beat_genres
     has_many :genres, through: :beat_genres
-    
+
+    def to_s
+        "#{self.title} (prod. #{self.user.username})"
+    end
 end
