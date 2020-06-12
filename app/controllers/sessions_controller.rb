49f2be6ca7a7cf90af7b66e645fb8e601af793fc
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-12 09:41:52 -0400

    updates signup.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,18 @@ class SessionsController < ApplicationController
     get '/signup' do 
         erb :'sessions/signup'
     end
+
+    post '/login' do
+        
+    end
+
+    post '/signup' do 
+        @u = User.new(username: params[:username], password: params[:password])
+        if @u.save
+            session[:user_id] = @u.id
+            redirect '/'
+        else
+            erb :'sessions/signup'
+        end
+    end
 end
