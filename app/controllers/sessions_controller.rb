1d25072d2e7bad7bed2fdda7d305cd017da60878
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-12 09:28:54 -0400

    wires up session views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,10 @@
 class SessionsController < ApllicationController
 
+    get '/login' do
+        erb :'sessions/login'
+    end
+
+    get '/signup' do 
+        erb :'sessions/signup'
+    end
 end
