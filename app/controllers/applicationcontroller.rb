6470b329c455f4363cfcd35e2e786336aa79e000
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-12 09:57:31 -0400

    adds helpers

--- app/controllers/applicationcontroller.rb
+++ app/controllers/applicationcontroller.rb
@@ -8,4 +8,19 @@ class ApplicationController < Sinatra::Base
     get '/' do
         erb :home
     end
+
+    helpers do
+        
+        def current_user
+            User.find_by(id: session[:user_id])
+        end
+
+        def logged_in?
+            !!current_user
+        end
+
+        def authenticate
+            redirect '/login' if !logged_in?
+        end
+    end
 end
