1fbfe67818bac93f87036126b71b6d8c0cce3b9b
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-14 12:58:56 -0400

    requires authentication for private pages

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -1,11 +1,13 @@
 class BeatsController < ApplicationController
 
-    get '/beats/' do 
+    get '/beats' do
+        authenticate 
         @beats = Beat.all
         erb :'beats/index'
     end
 
     get '/beats/:id' do 
+        authenticate
         @beat = Beat.find(params[:id])
         erb :'beats/show'
     end
