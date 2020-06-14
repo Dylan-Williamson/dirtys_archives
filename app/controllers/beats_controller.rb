c3994c9f04d1d67015b8bb43a26b2ba606cabe13
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-14 16:27:05 -0400

    --- Auto-Git Commit ---

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -6,9 +6,36 @@ class BeatsController < ApplicationController
         erb :'beats/index'
     end
 
+    get '/beats/new' do
+        erb :'beats/new'
+    end
+
+    post '/beats' do 
+        @beat = Beat.create(title: params[:title], genre_id: params[:genre_id])
+        redirect to '/beats/#{@beat.id}'
+    end
+
     get '/beats/:id' do 
         authenticate
         @beat = Beat.find(params[:id])
         erb :'beats/show'
+    end
+
+    get '/beats/:id/edit' do 
+        authenticate
+        @beat = Beat.find(params[:id])
+        erb :'beats/edit'
+    end
+
+    patch '/beats/:id' do 
+        @beat = Beat.find(params[:id])
+        @beat.title = params[:title]
+        @beat.genre_id = params[:genre_id]
+        @beat.save
+        redirect to '/beats/#{@beat.id}'
+    end
+
+    delete '/beats/:id' do 
+
     end
 end
