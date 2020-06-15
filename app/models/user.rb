ac586fdcd20386cd2f4e3d7838807f6d8dfc73c9
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-15 16:13:08 -0400

    --- Auto-Git Commit ---

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
     has_secure_password
     validates :username, uniqueness: true
+    validates :password, length: {in: 8..20}, unless: ->(u){ u.password.blank? }
 
     has_many :beats
     has_many :genres, through: :beats
