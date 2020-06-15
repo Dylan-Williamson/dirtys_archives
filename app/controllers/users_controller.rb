e7e02f82fc3ac8a146e148e1e268fcd07dadcf86
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-15 15:42:57 -0400

    --- Auto-Git Commit ---

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,33 @@ class UsersController < ApplicationController
         authenticate
         erb :"users/dashboard"
     end
+
+    get '/account/edit' do 
+        authenticate
+        erb :'users/edit_account'
+    end
+
+    patch '/account' do 
+        authenticate
+        @user = current_user
+        if !params[:password].empty?
+            @user.update(username: params[:username])
+            if @user.errors.any?
+                erb :'users/edit_account'
+            else
+                redirect '/dashboard'
+            end
+        else
+            if @user.authenticate(params[:password])
+                @user.update(username: params[:username], password: params[:new_password], password_confirmation: params[:new_password_confirmation])
+                if @user.errors.any?
+                    erb :'users/edit_account'
+                else
+                    redirect '/dashboard'
+                end
+            else
+                @user.errors.add(:password, "is incorrect")
+                erb :'users/edit_account'
+            end
+    end
 end
