e9191760edb50a600c0c5f8d16b8c0be4953af89
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-17 14:01:38 -0400

    --- Auto-Git Commit ---

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,8 +1,8 @@
 class Beat < ActiveRecord::Base
 
     belongs_to :user
-    has_many :beat_genres
-    has_many :genres, through: :beat_genres
+    has_one :beat_genre
+    # has_many :genres, through: :beat_genres
 
     def to_s
         "#{self.title} (prod. #{self.user.username})"
