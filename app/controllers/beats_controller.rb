ef514286caad887223cbddde7e6d2446f1de7187
dylan-williamson <dylwilliamson710@gmail.com>
2020-06-17 15:01:57 -0400

    --- Auto-Git Commit ---

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -15,25 +15,25 @@ class BeatsController < ApplicationController
         @beat = Beat.create(title: params[:title], genre_id: params[:genre_id], )
         redirect to '/dashboard'
     end
-
-    get '/beats/:id' do 
-        authenticate
-        @beat = Beat.find(params[:id])
-        erb :'beats/show'
-    end
-
+    
     get '/beats/:id/edit' do 
         authenticate
         @beat = Beat.find(params[:id])
         erb :'beats/edit'
     end
-
+    
     patch '/beats/:id' do 
         @beat = Beat.find(params[:id])
         @beat.title = params[:title]
         @beat.genre_id = params[:genre_id]
         @beat.save
         redirect to '/beats/#{@beat.id}'
+    end
+    
+    get '/beats/:id' do 
+        authenticate
+        @beat = Beat.find(params[:id])
+        erb :'beats/show'
     end
 
     delete '/beats/:id' do 
--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,7 +1,7 @@
 class Beat < ActiveRecord::Base
 
     belongs_to :user
-    has_one :genre
+    has_one :genre, through: :beat_genre
 
     def to_s
         "#{self.title} (prod. #{self.user.username})"
