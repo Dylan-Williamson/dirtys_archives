ed9d39a0df991b9455514cf26e25b052c4a482fa
dylan-williamson <dylwilliamson710@gmail.com>
2020-07-06 16:40:54 -0400

    --- Auto-Git Commit ---

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,7 +1,7 @@
 class Beat < ActiveRecord::Base
 
     belongs_to :user
-
+    validates :title, uniqueness: true, presence: true
     def to_s
         "#{self.title} (prod. #{self.user.username})"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
     has_secure_password
-    validates :username, uniqueness: true
+    validates :username, uniqueness: true, presence: true
     validates :password, length: {in: 8..20}, unless: ->(u){ u.password.blank? }
 
     has_many :beats
